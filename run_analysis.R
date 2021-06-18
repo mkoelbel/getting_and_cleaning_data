ae22a1194505d3267d6df2d4bb47bfde72bb2c6e
Koelbel, Maria <MKoelbel@geico.com>
2021-06-18 07:50:36 -0400

    clean up exraneous code; update file paths

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,32 @@
 
-# load libraries; set WD ------------------------------------------------------
 library(dplyr)
-
-setwd("C:/Users/u41030/Coursera/Getting and Cleaning Data/Final Project")
-
 
 # read in data ----------------------------------------------------------------
 
 # features / activity
-features <- read.delim(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",
+features <- read.delim(file = "UCI HAR Dataset/features.txt",
                        sep = " ",
                        header = F)
-activity_labels <- read.delim(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",
+activity_labels <- read.delim(file = "UCI HAR Dataset/activity_labels.txt",
                               sep = " ",
                               header = F)
 
 # train data
-x_train <- read.fwf(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt",
+x_train <- read.fwf(file = "UCI HAR Dataset/train/X_train.txt",
                     widths = c(17, rep(16, 560)),
                     header = F)
-y_train <- read.delim(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt",
+y_train <- read.delim(file = "UCI HAR Dataset/train/y_train.txt",
                       header = F)
-subject_train <- read.delim(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",
+subject_train <- read.delim(file = "UCI HAR Dataset/train/subject_train.txt",
                             header = F)
 
 # test data 
-x_test <- read.fwf(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt",
+x_test <- read.fwf(file = "UCI HAR Dataset/test/X_test.txt",
                    widths = c(17, rep(16, 560)),
                    header = F)
-y_test <- read.delim(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt",
+y_test <- read.delim(file = "UCI HAR Dataset/test/y_test.txt",
                      header = F)
-subject_test <- read.delim(file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",
+subject_test <- read.delim(file = "UCI HAR Dataset/test/subject_test.txt",
                            header = F)
 
 
@@ -60,17 +56,15 @@ subject <- rbind(subject_train, subject_test) %>%
   rename(subject_id = V1)
 
 data <- data %>% 
-  cbind(activity, subject) %>% 
-  select(-"activity_id") 
+  cbind(activity, subject)
 
 
 # create table of avgs by subject and activity --------------------------------
 
 avgs <- data %>% 
   group_by(activity, subject_id) %>% 
-  summarize(across(-c(activity, subject_id), mean))
-
-avgs <- data %>% 
-  group_by(activity, subject_id) %>% 
   summarize(across(1:66, mean))
+
+# write out to csv
+write_csv(avg, "avg_data.csv")
 
